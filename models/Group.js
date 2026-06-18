8b4b31183ab525781f44df64d1206d4ce48a575d
noahb-open <buchanan.nj@oundleschool.org.uk>
2026-06-18 17:57:00 +0100

    Update Group.js

--- models/Group.js
+++ models/Group.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const GroupSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  groupId: { type: String, required: true, unique: true }, // e.g., 'group_rocket_squad'
   creator: { type: String, required: true },
-  members: [{ type: String }],
-  timestamp: { type: Date, default: Date.now }
+  members: [{ type: String }] // Array of usernames allowed inside
 });
 
 module.exports = mongoose.model('Group', GroupSchema);
